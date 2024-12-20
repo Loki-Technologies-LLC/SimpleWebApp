4817acc4557caeb11c85dcbffe88397b0155215a
ajafry <ajafry@gmail.com>
2024-12-20 13:36:03 -0800

    modified to use app config and key vault

--- Program.cs
+++ Program.cs
@@ -1,16 +1,24 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.Azure.AppConfiguration.AspNetCore;
 using Azure.Identity;
+using DotNetEnv;
 
+// Once you deploy this to a WebApp, you have to ensure that you set an environment variable called 
+// "Endpoints:AppConfiguration" with the value of the App Configuration endpoint, which you can find from the
+// App Configuration resource
+// The SettingsPrefix is simply the prefix that keys stored in App Configuration will use to map the stored values
+// into a strongly typed class
 const string AppConfigConnectionStringKey = "Endpoints:AppConfiguration";
 const string SettingsPrefix = "MyApp:Settings";
 
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Retrieve the endpoint
 string endpoint = builder.Configuration.GetValue<string>(AppConfigConnectionStringKey)
     ?? throw new InvalidOperationException($"The setting `{AppConfigConnectionStringKey}` was not found.");
 
+var clientId = Environment.GetEnvironmentVariable("AZURE_CLIENT_ID");
+
+var defaultAzureCredentials = new DefaultAzureCredential();
 // Load configuration from Azure App Configuration (Azure CLI must be logged in for this to work and your ID must have access
 // to the App Configuration instance.
 // In production, the Managed Identity for the App Service will have this access, as you would need to configure that as well
@@ -18,10 +26,10 @@ builder.Configuration.AddAzureAppConfiguration(options =>
 {
     options.ConfigureKeyVault(keyVaultOptions =>
     {
-        keyVaultOptions.SetCredential(new DefaultAzureCredential());
+        keyVaultOptions.SetCredential(defaultAzureCredentials);
     });
-    
-    options.Connect(new Uri(endpoint), new DefaultAzureCredential());
+
+    options.Connect(new Uri(endpoint), defaultAzureCredentials);
 });
 
 // Add services to the container.
